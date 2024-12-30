18e01ab1ed7f367322de2d9b3ac880792683d5a9
Damian Ianni <iannigabrieldamian@gmail.com>
2024-12-30 23:21:29 +0100

    Third exercise completed

---
+++ Tests/GettingTheMiddleFinger.test.ts
@@ -0,0 +1,5 @@
+import { gettingTheMiddleFinger } from "../Exercises/GettingTheMiddleFinger";
+
+test("Get the middle/(s) character(s) of a string", () => {
+  expect(gettingTheMiddleFinger("test")).toBe("es");
+});

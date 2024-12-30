1c16b9e724ae79c8ac91b46898e947511b80b68a
Damian Ianni <damianianni@Damians-MacBook-Pro.local>
2024-12-30 23:29:48 +0100

    just a little fix

--- Exercises/GettingTheMiddleFinger.ts
+++ Exercises/GettingTheMiddleFinger.ts
@@ -1,6 +1,5 @@
 /*
 You are going to be given a non-empty string. Your job is to return the middle character(s) of the string.
-
 If the string's length is odd, return the middle character.
 If the string's length is even, return the middle 2 characters.
 
